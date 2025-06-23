0535992a6098bcb10f99db49f5209d61099eaff3
Nymora-World <nymoraworld@gmail.com>
2025-06-23 10:19:30 +0530

    added update route

--- src/v1/routes/fourm.route.js
+++ src/v1/routes/fourm.route.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { Protect } from "../middleware/auth.middleware.js";
-import { createFourmPost, getPostByClass } from "../controllers/fourm.controller.js";
+import {
+  createFourmPost,
+  getPostByClass,
+} from "../controllers/fourm.controller.js";
 
 const router = express.Router();
 
 router.post("/create/fourm/post", Protect, createFourmPost);
-router.get("/get/fourm/post/:classname", Protect, getPostByClass);
+router.get("/get/fourm/post/:classname", getPostByClass);
 
 export default router;
